5b26efa5822c6a16d0f9e779a99fb7da5160e115
AhmedKhaled6900 <you@example.com>
2023-06-07 21:57:32 +0300

    first commit

--- js/new project.js
+++ js/new project.js
@@ -36,3 +36,23 @@ const fun=()=>{
     })
 }
 fun()
+const fun3=()=>{
+    let alis=Array.from(document.querySelectorAll(".project3 ul li"))  
+    console.log(alis)
+    let viewedfeature=Array.from(document.querySelectorAll(".project3 .view3")) 
+    console.log(viewedfeature)
+    alis.forEach((li)=>{
+    li.addEventListener(("click"),(e)=>{
+     alis.forEach((li)=>{
+        li.classList.remove("active")
+     })   
+     e.currentTarget.classList.add("active")
+     viewedfeature.forEach((view)=>{
+    view.classList.remove("active")
+     })
+        document.querySelector(e.currentTarget.dataset.content).classList.add("active")
+    
+    })
+    })
+}
+fun3()
