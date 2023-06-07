ff69dfc882f26d8a58ef50e43fdd3884ca69b0ec
AhmedKhaled6900 <you@example.com>
2023-06-07 21:14:26 +0300

    first commit

---
+++ js/new project.js
@@ -0,0 +1,38 @@
+let alis=Array.from(document.querySelectorAll(".project1 ul li"))  
+console.log(alis)
+let viewedfeature=Array.from(document.querySelectorAll(".project1 .view")) 
+console.log(viewedfeature)
+alis.forEach((li)=>{
+li.addEventListener(("click"),(e)=>{
+ alis.forEach((li)=>{
+    li.classList.remove("active")
+ })   
+ e.currentTarget.classList.add("active")
+ viewedfeature.forEach((view)=>{
+view.classList.remove("active")
+ })
+    document.querySelector(e.currentTarget.dataset.content).classList.add("active")
+
+})
+})
+
+const fun=()=>{
+    let alis=Array.from(document.querySelectorAll(".project2 ul li"))  
+    console.log(alis)
+    let viewedfeature=Array.from(document.querySelectorAll(".project2 .view2")) 
+    console.log(viewedfeature)
+    alis.forEach((li)=>{
+    li.addEventListener(("click"),(e)=>{
+     alis.forEach((li)=>{
+        li.classList.remove("active")
+     })   
+     e.currentTarget.classList.add("active")
+     viewedfeature.forEach((view)=>{
+    view.classList.remove("active")
+     })
+        document.querySelector(e.currentTarget.dataset.content).classList.add("active")
+    
+    })
+    })
+}
+fun()
